70950136dd74cb8e2f9189fc0bcb57b7edf9c740
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-11-29 19:55:47 +0900

    viewの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,message: 'Password error' }
+  end
 end
