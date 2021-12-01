7acddd2cd05cbe40fe82a9b6bb6e31f72d742e43
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-12-01 19:42:34 +0900

    adminの制約

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :redirect_index, only: [:new, :create]
 
   def index
   end
@@ -22,5 +23,9 @@ class EventsController < ApplicationController
   private
   def event_params
     params.require(:event).permit(:event_title, :place, :prefecture_id, :genre_id, :date, :image).merge(user_id: current_user.id)
+  end
+  
+  def redirect_index
+    redirect_to root_path if current_user.admin = 0
   end
 end
