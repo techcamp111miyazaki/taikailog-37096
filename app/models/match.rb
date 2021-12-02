c0e6dd52de2085beb14b0cfef394109a829670f2
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-12-02 18:43:21 +0900

    投稿機能の実装

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,11 @@
 class Match < ApplicationRecord
   validates :turn_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :field_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :order_number, presence: true
+  validates :order_number, presence: true, numericality: { only_integer: true, message: "input only half-numbers" }
   validates :player_name_1, presence: true
   validates :belongs_1, presence: true
+  # validates :score_1, numericality: { message: "input only half-numbers" }
+  # validates :score_2, numericality: { message: "input only half-numbers" }
 
   belongs_to :user
   belongs_to :event
