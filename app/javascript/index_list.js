56b32cfdb34e1f09a04ac3d183256351e3d7704c
techcamp111miyazaki <kirohi19980715@gmail.com>
2021-12-21 07:12:05 +0900

    Update index_list.js

--- app/javascript/index_list.js
+++ app/javascript/index_list.js
@@ -1,11 +1,13 @@
 window.addEventListener('load', function(){
-  const dropdownItem = document.querySelectorAll(".dropdown-item")
-  const currentList = document.getElementById("current-list")
+  if (document.getElementById("current-list")){
+    const dropdownItem = document.querySelectorAll(".dropdown-item")
+    const currentList = document.getElementById("current-list")
 
-  dropdownItem.forEach(function(item){
-    item.addEventListener('click', function(){
-      const value = item.innerHTML
-      currentList.innerHTML = value
+    dropdownItem.forEach(function(item){
+      item.addEventListener('click', function(){
+        const value = item.innerHTML
+        currentList.innerHTML = value
+      })
     })
-  })
+  }
 })
